aaa90baa67ca73b424fe1e12a95d21da122bbd30
keksgauner <darklupo5.2003@gmail.com>
2020-07-14 19:19:51 +0200

    Initial commit

---
+++ src/main/java/de/byaple/actionbar/Setup.java
@@ -0,0 +1,51 @@
+package de.byaple.actionbar;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import com.connorlinfoot.actionbarapi.ActionBarAPI;
+import com.google.common.eventbus.Subscribe;
+import com.plotsquared.core.api.PlotAPI;
+import com.plotsquared.core.events.PlayerEnterPlotEvent;
+import com.plotsquared.core.plot.Plot;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Setup extends JavaPlugin {
+
+    public void onEnable() {
+        PlotAPI api = new PlotAPI();
+        api.registerListener(this);
+    }
+
+    @Subscribe
+    public void plotEnter(PlayerEnterPlotEvent event) {
+                Player player = Bukkit.getPlayer(event.getPlotPlayer().getName());
+                Plot plot = event.getPlot();
+                int plotX = event.getPlot().getId().getX();
+                int plotY = event.getPlot().getId().getY();
+
+                if (plot.getOwners().isEmpty()) {
+                    sendActionbar(player, "&aFreies Plot" + " &7| " + plotX + ";" + plotY);
+                } else {
+                    String plot_owner = getName(plot);
+                        sendActionbar(player, "&3" + plot_owner + " &7| " + plotX + ";" + plotY);
+                }
+    }
+
+
+    ArrayList<UUID> uuid = new ArrayList<>();
+
+    public static String getName(Plot plot) {
+        Set<UUID> uuid = plot.getOwners();
+        UUID firstuuid = uuid.iterator().next();
+        return Bukkit.getOfflinePlayer(firstuuid).getName();
+    }
+
+    public static void sendActionbar(Player player, String message) {
+        ActionBarAPI.sendActionBar(player, ChatColor.translateAlternateColorCodes('&', message));
+    }
+}
