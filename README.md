aca63086b23f215746353d99f0ee654e9f9c70f2
keksgauner <darklupo5.2003@gmail.com>
2022-07-25 19:16:09 +0200

    Added config to the plugin

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # PlotSquared-ActionBar
 
-A PlotSquared v6 addon that displays the owners in the action bar.
+Native server version: 1.16.5
 
+### A PlotSquared v6 addon that displays the owners in the action bar.
+### Full configuration
+```
+# Available placeholders:
+# %plotX%  - the x of the plotID
+# %plotY%  - the y of the plotID
+# %owner%  - the owner of the plot
+plot:
+  free: "&aFree Plot &7| %plotX%;%plotY%"
+  owner: "&3%owner% &7| %plotX%;%plotY%"
+  yourself: "&3Your Plot &7| %plotX%;%plotY%"
+```
 
-Support
-Discord: KeksGauner#0624
+### Example
+#### You can return the owner and the property number belonging to it
+![image](https://cloud.rakutt.eu/s/XCmskxdw4SCqBPo/preview)
+![image](https://cloud.rakutt.eu/s/GZHpSdesBPWCW8s/preview)
+#### You can determine what should be displayed for a free plot
+![image](https://cloud.rakutt.eu/s/YEH57828iGbiT5o/preview)
+#### It also distinguishes whether it is your plot or someone else's
+![image](https://cloud.rakutt.eu/s/WYcKHNXw7G3ZKQ7/preview)
+
+#### Support by Discord: KeksGauner#0624
--- src/main/java/de/kleckzz/actionbar/PlotSquaredActionBar.java
+++ src/main/java/de/kleckzz/actionbar/PlotSquaredActionBar.java
@@ -1,7 +1,8 @@
-package de.byaple.actionbar;
+package de.kleckzz.actionbar;
 
 import java.util.Set;
 import java.util.UUID;
+import java.util.logging.Level;
 
 import com.google.common.eventbus.Subscribe;
 import com.plotsquared.core.PlotAPI;
@@ -14,9 +15,24 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Setup extends JavaPlugin {
+public class PlotSquaredActionBar extends JavaPlugin {
+    private String plot_free;
+    private String plot_owner;
+    private String plot_owner_yourself;
 
     public void onEnable() {
+        if(!this.getServer().getPluginManager().isPluginEnabled("PlotSquared")) {
+            this.getLogger().log(Level.SEVERE, "PlotSquared not found! To use this plugin you need to install PlotSquared!");
+            this.getPluginLoader().disablePlugin(this);
+        }
+
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+
+        this.plot_free = getConfig().getString("plot.free");
+        this.plot_owner = getConfig().getString("plot.owner");
+        this.plot_owner_yourself = getConfig().getString("plot.yourself");
+
         PlotAPI api = new PlotAPI();
         api.registerListener(this);
     }
@@ -31,13 +47,13 @@ public class Setup extends JavaPlugin {
         int plotY = event.getPlot().getId().getY();
 
         if (plot.getOwners().isEmpty())
-            sendActionbar(player, "&aFree Plot" + " &7| " + plotX + ";" + plotY);
+            sendActionbar(player, plot_free.replace("%plotX%", String.valueOf(plotX)).replace("%plotY%", String.valueOf(plotY)));
         else {
             String owner = getName(plot) + "";
             if (owner.equalsIgnoreCase(player.getName()))
-                sendActionbar(player, "&3Your Plot" + " &7| " + plotX + ";" + plotY);
+                sendActionbar(player, plot_owner_yourself.replace("%plotX%", String.valueOf(plotX)).replace("%plotY%", String.valueOf(plotY)).replace("%owner%", String.valueOf(owner)));
             else
-                sendActionbar(player, "&3" + owner + " &7| " + plotX + ";" + plotY);
+                sendActionbar(player, plot_owner.replace("%plotX%", String.valueOf(plotX)).replace("%plotY%", String.valueOf(plotY)).replace("%owner%", String.valueOf(owner)));
         }
     }
 
